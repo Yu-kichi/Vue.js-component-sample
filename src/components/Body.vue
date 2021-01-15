73bc7fbb14a768ec57a459acae0ac7109f146572
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-01-15 11:31:52 +0900

    first commit

---
+++ src/components/Body.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ text }}</p>
+    <p><button @click="increment">+1</button> {{ count }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Body',
+      text: 'Have a good day!',
+      count: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.count += 1
+      this.$emit('add', this.count)
+    }
+  }
+}
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :username='name' v-model="InputData.condition">
+      <template v-slot:message>
+        <p>Let's enjoy programming!</p>
+      </template>
+    </Header>
+    <p>condition : {{ InputData.condition }} </p>
+
+    <Body @add="add1"></Body>
+
+    <Body @add="add2"></Body>
+    <p>total : {{ totalcount }} </p>
+    <button @click="componentName = 'Header'">Header</button>
+    <button @click="componentName = 'Body'">Body</button>
+    <keep-alive>
+      <component :is="componentName"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from '@/components/Header.vue'
+import Body from '@/components/Body.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Header,
+    Body
+  },
+  data () {
+    return {
+      name: 'kiyokiyo',
+      count1: 0,
+      count2: 0,
+      totalcount: 0,
+      componentName: 'Header',
+      InputData: {
+        condition: ''
+      }
+    }
+  },
+  methods: {
+    add1 (count) {
+      this.count1 = count
+      this.totalcount = this.count1 + this.count2
+    },
+    add2 (count) {
+      this.count2 = count
+      this.totalcount = this.count1 + this.count2
+    }
   }
 }
+
 </script>
+
+<style scoped>
+  p {
+    color: green
+  }
+
+</style>
